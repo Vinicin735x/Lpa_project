c215b5b4de42ce3331cc5084d67af97a14eb7b96
Vinicin735x <vini08.mantovani@gmail.com>
2023-08-01 15:18:06 -0300

    quem vc pensa que é

---
+++ src/components/ListMarker/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import { Marker } from '../Marker'
+import { styles } from './styles'
+export interface ITextMarker {
+    textMarker:string
+}
+export function ListMarker({ textMarker }: ITextMarker) {
+    return (
+        <View style={styles.listMarker}>
+            <Marker />
+            <Text style={styles.textMarker} >{textMarker}</Text>
+        </View>
+    )
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    primary: '#0558FA',
+    white: '#FFF',
+    black: '#000'
+}
